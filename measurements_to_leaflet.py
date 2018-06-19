22d126dc6270e02459a34bd4826c6fc4971524a9
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2018-06-19 15:31:53 +0100

    Update with 2017 data

---
+++ measurements_to_leaflet.py
@@ -0,0 +1,30 @@
+import csv
+from ostn02python import OSGB, OSTN02
+
+if __name__=="__main__":
+
+	outPollution = open("outPollution.js", "wt")
+
+	receptorPosDict = {}
+	for row in csv.DictReader(open("measuredno2.csv")):
+		
+		receptorNum = row["Site ID"]
+		(x,y,h) = OSTN02.OSGB36_to_ETRS89 (float(row["X"]), float(row["Y"]))
+		(gla, glo) = OSGB.grid_to_ll(x, y)
+
+		if len(row["2017"]) == 0: continue
+
+		m2017 = float(row["2017"])
+
+		popupHtml = "<b>Receptor {}</b>".format(receptorNum)
+
+		for year in range(2012, 2018):
+			if len(row[str(year)]) == 0: continue
+			popupHtml += "<br/>NO2 {}: {}".format(year, float(row[str(year)]))
+		
+		outPollution.write(("\tL.marker([{}, {}], {{icon: caution{}sIcon}}).addTo(mymap).bindPopup(\""
+			+"{}\");\n").format(
+			gla, glo, int(m2017/10)*10, popupHtml))
+
+	outPollution.close()
+
