010cbf6b2b69085ffb1aa0f26648912c646d65af
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2018-06-20 11:51:04 +0100

    Show new site locations

--- measurements_to_leaflet.py
+++ measurements_to_leaflet.py
@@ -12,19 +12,26 @@ if __name__=="__main__":
 		(x,y,h) = OSTN02.OSGB36_to_ETRS89 (float(row["X"]), float(row["Y"]))
 		(gla, glo) = OSGB.grid_to_ll(x, y)
 
-		if len(row["2017"]) == 0: continue
+		if len(row["2017"]) > 0:
 
-		m2017 = float(row["2017"])
+			m2017 = float(row["2017"])
 
-		popupHtml = "<b>Receptor {}</b>".format(receptorNum)
+			popupHtml = "<b>Receptor {}</b>".format(receptorNum)
 
-		for year in range(2012, 2018):
-			if len(row[str(year)]) == 0: continue
-			popupHtml += "<br/>NO2 {}: {}".format(year, float(row[str(year)]))
+			for year in range(2012, 2018):
+				if len(row[str(year)]) == 0: continue
+				popupHtml += "<br/>NO2 {}: {}".format(year, float(row[str(year)]))
 		
-		outPollution.write(("\tL.marker([{}, {}], {{icon: caution{}sIcon}}).addTo(mymap).bindPopup(\""
-			+"{}\");\n").format(
-			gla, glo, int(m2017/10)*10, popupHtml))
+			outPollution.write(("\tL.marker([{}, {}], {{icon: caution{}sIcon}}).addTo(mymap).bindPopup(\""
+				+"{}\");\n").format(
+				gla, glo, int(m2017/10)*10, popupHtml))
 
+		else:
+			popupHtml = "<b>Receptor {}</b>".format(receptorNum)
+		
+			outPollution.write(("\tL.marker([{}, {}], {{icon: negligibleIcon}}).addTo(mymap).bindPopup(\""
+				+"{}\");\n").format(
+				gla, glo, popupHtml))
+			
 	outPollution.close()
 
