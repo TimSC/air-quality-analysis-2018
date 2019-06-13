64a163a1b406f708a66a6f510647d9bfd49b7e5c
Tim Sheerman-Chase <tim2009@sheerman-chase.org.uk>
2019-06-13 14:07:49 +0100

    Update with new data

--- gbosconvert.py
+++ gbosconvert.py
@@ -6,25 +6,28 @@ if __name__=="__main__":
 	outMeasurements = open("outMeasurements.js", "wt")
 
 
-	for row in csv.DictReader(open("actualno2.csv")):
+	for row in csv.DictReader(open("measuredno2-2019.csv")):
 
 		#print (row)
 
 		(x,y,h) = OSTN02.OSGB36_to_ETRS89 (float(row["X"]), float(row["Y"]))
 		(gla, glo) = OSGB.grid_to_ll(x, y)
 		print (row["Site ID"], gla, glo)
-		if len(row["2016"])==0: continue
+		if len(row["2018"])==0: continue
 
-		outMeasurements.write(("\tL.marker([{}, {}], {{icon: caution{}sIcon}}).addTo(mymap3).bindPopup(\""
-			+"<b>Receptor {}</b><br/>"
-			+"NO2 2012: {}<br/>"
-			+"NO2 2013: {}<br/>"
-			+"NO2 2014: {}<br/>"
-			+"NO2 2015: {}<br/>"
-			+"NO2 2016: {}<br/>"
-			+"\");\n").format(
-			gla, glo, int(float(row["2016"])/10)*10, 
-			row["Site ID"], row["2012"], row["2013"], row["2014"], row["2015"], row["2016"]))
+		vals = [gla, glo, int(float(row["2018"])/10)*10, 
+			row["Site ID"]]
+		fmt = ("\tL.marker([{}, {}], {{icon: caution{}sIcon}}).addTo(mymap3).bindPopup(\""
+			+"<b>Receptor {}</b><br/>")
+
+		for y in range(2012,2019):
+			if len(row[str(y)]) == 0: continue
+			fmt += "NO2 {}: {}<br/>".format(y, "{}")
+			vals.append(row[str(y)])
+
+		fmt += "\");\n"
+
+		outMeasurements.write(fmt.format(*vals))
 
 	outMeasurements.close()
 
